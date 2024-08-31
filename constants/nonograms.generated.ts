704acdc9b71f6148130937802272949fafee9afb
Lukas Gjetting <lukasgjetting@gmail.com>
2024-08-31 20:46:48 +0200

    Add 7th row to house

--- constants/nonograms.generated.ts
+++ constants/nonograms.generated.ts
@@ -1,6 +1,6 @@
 export const NonogramSources = {
   "intro.simple": "x-x\nxxx\nx--",
-  "intro.house": "---x---\n--xxx--\n-xxxxx-\nxxxx-xx\n-x-xxx-\n-x-xxx-"
+  "intro.house": "---x---\n--xxx--\n-xxxxx-\nxxxx-xx\n-x-xxx-\n-x-xxx-\nxxxxxxx"
 } as const;
 
 export type NonogramKey = keyof typeof NonogramSources;
