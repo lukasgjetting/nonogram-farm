e41a5ac56431dedb091f1a0916b2b57e86818df5
Lukas Gjetting <lukasgjetting@gmail.com>
2024-09-02 00:03:07 +0200

    Add all the other seed images

--- src/constants/seeds.ts
+++ src/constants/seeds.ts
@@ -48,12 +48,77 @@ export const SEED_STAGE_IMAGES: Record<
       heightPercentage: 40,
     },
   ],
+  carrot: [
+    {
+      source: require("@/assets/images/seed-stages/carrot-1.png"),
+      widthPercentage: 15,
+      heightPercentage: 15,
+    },
+    {
+      source: require("@/assets/images/seed-stages/carrot-2.png"),
+      widthPercentage: 30,
+      heightPercentage: 30,
+    },
+    {
+      source: require("@/assets/images/seed-stages/carrot-3.png"),
+      widthPercentage: 40,
+      heightPercentage: 40,
+    },
+    {
+      source: require("@/assets/images/seed-stages/carrot-4.png"),
+      widthPercentage: 55,
+      heightPercentage: 55,
+    },
+  ],
+  tomato: [
+    {
+      source: require("@/assets/images/seed-stages/tomato-1.png"),
+      widthPercentage: 20,
+      heightPercentage: 20,
+    },
+    {
+      source: require("@/assets/images/seed-stages/tomato-2.png"),
+      widthPercentage: 30,
+      heightPercentage: 30,
+    },
+    {
+      source: require("@/assets/images/seed-stages/tomato-3.png"),
+      widthPercentage: 45,
+      heightPercentage: 45,
+    },
+    {
+      source: require("@/assets/images/seed-stages/tomato-4.png"),
+      widthPercentage: 70,
+      heightPercentage: 70,
+    },
+  ],
+  pineapple: [
+    {
+      source: require("@/assets/images/seed-stages/pineapple-1.png"),
+      widthPercentage: 15,
+      heightPercentage: 15,
+    },
+    {
+      source: require("@/assets/images/seed-stages/pineapple-2.png"),
+      widthPercentage: 20,
+      heightPercentage: 20,
+    },
+    {
+      source: require("@/assets/images/seed-stages/pineapple-3.png"),
+      widthPercentage: 28,
+      heightPercentage: 28,
+    },
+    {
+      source: require("@/assets/images/seed-stages/pineapple-4.png"),
+      widthPercentage: 45,
+      heightPercentage: 65,
+    },
+  ],
 };
 
 export const SEED_GROWTH_TIME: Record<SeedType, number> = {
   lettuce: 30000,
-  carrot: 40000,
-  tomato: 50000,
-  watermelon: 60000,
-  pineapple: 70000,
+  carrot: 60000,
+  tomato: 120000,
+  pineapple: 300000,
 };
